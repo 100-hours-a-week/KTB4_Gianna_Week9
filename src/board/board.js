e9f8c50aafc267a22c8426aba9b58d9fb19d97f6
Stella <yeobinlee1210@gmail.com>
2026-06-28 10:40:39 +0900

    feat(board): 게시물 목록 화면에 띄우는 기능 추가

--- src/board/board.js
+++ src/board/board.js
@@ -1,4 +1,7 @@
-(async function () {
+const postList = [];
+showPostList();
+
+async function getPostList  () {
     try{
         const response = await fetch('http://localhost:8080/posts', {
             method: 'GET',
@@ -13,14 +16,59 @@
         }
 
         const data = await response.json();
-        console.log(data.data)
+        data.data.postList.forEach(posts => {
+            postList.push(posts);
+        });
     }catch(error){
         console.error('로그인 중 오류 발생:', error);
     }
-}());
+}
 
-const postContainer = document.getElementsByClassName("common-container");
+async function showPostList () {
 
-postContainer[0].addEventListener('click' ,(event)=>{
-    window.location.href = "./boardView/boardView.html";
-});
+    await getPostList();
+    postList.forEach((post)=>{
+        makePostView(post)
+    })
+   
+    // postContainer[0].addEventListener('click' ,(event)=>{
+    //     window.location.href = "./boardView/boardView.html";
+    // });
+}
+
+const makePostView = (post) =>{
+ const postDiv = document.createElement("div");
+    postDiv.classList.add('common-container');
+
+    const title = document.createElement('h3');
+    title.id ="postListTitle"
+    title.textContent = post.title;
+    postDiv.append(title)
+
+    const likes = document.createElement('h4');
+    likes.id ="postListLikes"
+    likes.textContent = `좋아요 ${0}`;
+    postDiv.append(likes)
+
+    const commentCount = document.createElement('h4');
+    commentCount.id ="postListCommentCount"
+    commentCount.textContent = `댓글 ${0}`;
+    postDiv.append(commentCount)
+
+    const views = document.createElement('h4');
+    views.id ="postListViews"
+    views.textContent = `조회수 ${0}`;
+    postDiv.append(views)
+
+    const date = document.createElement('h4');
+    date.id = "postListDate"
+    date.textContent = post.createdAt
+    postDiv.append(date)
+
+    const author = document.createElement('h4');
+    author.id = "postListAuthor"
+    author.textContent = post.author;
+    postDiv.append(author)
+
+    document.body.appendChild(postDiv);
+}
