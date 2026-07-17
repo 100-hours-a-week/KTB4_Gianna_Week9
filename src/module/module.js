dcbc904a188a1420e44f947abf2f4f9bc59231ce
Stella <yeobinlee1210@gmail.com>
2026-07-17 23:00:36 +0900

    feat(module): favicon 추가

--- src/module/module.js
+++ src/module/module.js
@@ -8,11 +8,17 @@ export const getUserId = async () =>{
     return cookie.value;
 }
 
+import { requestCsrfAPIJsonResponse } from "../api/csrf.js";
 export const getUser = async(userId) =>{
+const csrf = await requestCsrfAPIJsonResponse();
+
     try{
         const response = await fetch(`http://localhost:8080/users/${userId}`, {
             method: 'GET',
-            credentials:"include"    
+            credentials:"include", 
+            headers:{
+                [csrf.headerName] : csrf.token                
+            }   
         });
 
         if (!response.ok) {
@@ -26,6 +32,7 @@ export const getUser = async(userId) =>{
         console.error('오류 발생:', error);
     }
 } 
+
 
 export const formalizeDate = (datetime) =>{
     const [date, time] = datetime.split("T");
